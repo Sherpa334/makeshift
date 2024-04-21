17f1144bd639758aeabff72b2e51688aa24915da
Stanley He <she8@buffalo.edu>
2024-04-21 18:54:11 -0400

    Added chat history

--- makeshift/website/consumers.py
+++ makeshift/website/consumers.py
@@ -2,6 +2,8 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
+chat_history = []
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_group_name = 'test'
@@ -10,6 +12,13 @@ class ChatConsumer(WebsocketConsumer):
             self.channel_name
         )
         self.accept()
+
+        for message in chat_history:
+            self.send(text_data=json.dumps({
+                'type': 'chat',
+                'message': message['message'],
+                'username': message['username'],
+            }))
 
         self.send(text_data=json.dumps({
             'type': 'connection_established',
@@ -20,6 +29,12 @@ class ChatConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         username = text_data_json.get('username', 'Guest')  # Use 'Guest' if username is not provided
+
+        chat_history.append({
+            'message': message,
+            'username': username,
+        })
+        
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
